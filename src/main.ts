39e2670602c19c02710341f789098101ff52e44b
Tu Hoang <mail@thoang.me>
2021-02-07 21:35:07 +0100

    fix aws-exports try number 3

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import Amplify from "aws-amplify";
-import awsExports from "./aws-exports.js";
+import awsExports from "@/aws-exports";
 
 Amplify.configure(awsExports);
 Vue.config.productionTip = false;
