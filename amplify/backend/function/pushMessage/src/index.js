0421429ea6d9573a5818b7ba0b646054bb5dbf30
thoang <tht157@hotmail.de>
2021-03-04 18:21:07 +0100

    improve permission handlling for lambda - change to iam instead of api key - api key is now allowed for read only operations - update dependencies for lambda functions

--- amplify/backend/function/pushMessage/src/index.js
+++ amplify/backend/function/pushMessage/src/index.js
@@ -1,22 +1,24 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const axios = require("axios");
 const gql = require("graphql-tag");
 const graphql = require("graphql");
+const AWS = require("aws-sdk");
+const https = require("https");
+const URL = require("url").URL;
 const { print } = graphql;
 
-const mutation = gql`
-    mutation createMessage($input: CreateMessageInput!) {
-        createMessage(input: $input) {
-            id
-            content
-            timestamp
-            messageStatus
-            _version
-            _lastChangedAt
-            _deleted
-        }
+const mutation = gql`mutation createMessage($input: CreateMessageInput!) {
+    createMessage(input: $input) {
+        id
+        content
+        timestamp
+        messageStatus
     }
+  }
   `;
+
+const ENDPOINT_URI = process.env.API_FENSTELLA_GRAPHQLAPIENDPOINTOUTPUT;
+const ENDPOINT_HOST = new URL(process.env.API_FENSTELLA_GRAPHQLAPIENDPOINTOUTPUT).hostname.toString();
+const REGION = process.env.REGION;
 
 /* Amplify Params - DO NOT EDIT
     API_FENSTELLA_GRAPHQLAPIENDPOINTOUTPUT
@@ -28,24 +30,50 @@ Amplify Params - DO NOT EDIT */
 
 exports.handler = async function(event) {
     try {
-        const requestBody = JSON.parse(event.body);
-        if (requestBody.text != null && requestBody.text.length > 0) {
-            const response = await axios({
-                url: process.env.API_FENSTELLA_GRAPHQLAPIENDPOINTOUTPUT,
-                method: "post",
-                headers: {
-                    "x-api-key": process.env.API_FENSTELLA_GRAPHQLAPIKEYOUTPUT
-                },
-                data: {
-                    query: print(mutation),
-                    variables: {
-                        input: {
-                            content: requestBody.text,
-                            timestamp: new Date().valueOf(),
-                            messageStatus: "NEW"
-                        }
+        let text = null;
+        if (event.queryStringParameters != null && event.queryStringParameters.text != null && event.queryStringParameters.text.length > 0) {
+            text = event.queryStringParameters.text;
+        } else if (event.body != null) {
+            const requestBody = JSON.parse(event.body);
+            if (requestBody.text != null && requestBody.text.length > 0) {
+                text = requestBody.text;
+            }
+        }
+
+        if (text != null && text.length > 0) {
+            const req = new AWS.HttpRequest(ENDPOINT_URI, REGION);
+
+            req.method = "POST";
+            req.path = "/graphql";
+            req.headers.host = ENDPOINT_HOST;
+            req.headers["Content-Type"] = "application/json";
+            req.body = JSON.stringify({
+                query: print(mutation),
+                variables: {
+                    input: {
+                        content: text,
+                        timestamp: new Date().valueOf(),
+                        messageStatus: "NEW"
                     }
                 }
+            });
+
+            const signer = new AWS.Signers.V4(req, "appsync", true);
+            signer.addAuthorization(AWS.config.credentials, AWS.util.date.getDate());
+
+            const data = await new Promise((resolve, reject) => {
+                const httpRequest = https.request({ ...req, host: ENDPOINT_HOST }, (result) => {
+                    result.on("data", (data) => {
+                        resolve(JSON.parse(data.toString()));
+                    });
+
+                    result.on("error", (e) => {
+                        reject(e);
+                    });
+                });
+
+                httpRequest.write(req.body);
+                httpRequest.end();
             });
 
             return {
@@ -55,7 +83,7 @@ exports.handler = async function(event) {
                     "Access-Control-Allow-Origin": "*",
                     "Access-Control-Allow-Headers": "*"
                 },
-                body: JSON.stringify({ message: "done", body: response.data })
+                body: JSON.stringify({ message: "done", body: data })
             };
         }
 
